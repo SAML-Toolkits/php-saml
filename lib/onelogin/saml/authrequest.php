bbed6a968a3420d18260f5a266aeba813b07d8aa
Earl Miles <merlin@logrus.com>
2010-12-06 12:11:59 -0800

    Fix incorrect use of constants.

--- lib/onelogin/saml/authrequest.php
+++ lib/onelogin/saml/authrequest.php
@@ -6,9 +6,12 @@
       $id                = $this->generateUniqueID(42);
       $issue_instant     = $this->getTimestamp();
 
-      $request = 
+      global $const_assertion_consumer_service_url;
+      global $const_issuer;
+      global $const_name_identifier_format;
+      $request =
         "<samlp:AuthnRequest xmlns:samlp=\"urn:oasis:names:tc:SAML:2.0:protocol\" ID=\"$id\" Version=\"2.0\" IssueInstant=\"$issue_instant\" ProtocolBinding=\"urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST\" AssertionConsumerServiceURL=\"".$const_assertion_consumer_service_url."\">".
-        "<saml:Issuer xmlns:saml=\"urn:oasis:names:tc:SAML:2.0:assertion\">".$cosnt_issuer."</saml:Issuer>\n".
+        "<saml:Issuer xmlns:saml=\"urn:oasis:names:tc:SAML:2.0:assertion\">".$const_issuer."</saml:Issuer>\n".
         "<samlp:NameIDPolicy xmlns:samlp=\"urn:oasis:names:tc:SAML:2.0:protocol\" Format=\"".$const_name_identifier_format."\" AllowCreate=\"true\"></samlp:NameIDPolicy>\n".
         "<samlp:RequestedAuthnContext xmlns:samlp=\"urn:oasis:names:tc:SAML:2.0:protocol\" Comparison=\"exact\">".
         "<saml:AuthnContextClassRef xmlns:saml=\"urn:oasis:names:tc:SAML:2.0:assertion\">urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport</saml:AuthnContextClassRef></samlp:RequestedAuthnContext>\n".
@@ -22,7 +25,7 @@
 
       return $this->user_settings->idp_sso_target_url."?SAMLRequest=".$encoded_request;
     }
-    
+
     private function generateUniqueID($length) {
       $chars = "abcdef0123456789";
       $chars_len = strlen($chars);
@@ -31,7 +34,7 @@
         $uniqueID .= substr($chars,rand(0,15),1);
       return $uniqueID;
     }
-    
+
     private function getTimestamp() {
       date_default_timezone_set('UTC');
       return strftime("%Y-%m-%dT%H:%M:%SZ");
