5aaa8a89a80f23d47243e98eb2db1bae1402df01
Joe Rozner <joe.rozner@onelogin.com>
2012-05-11 14:12:50 -0700

    fixed #7 make sure files are only required once

--- src/OneLogin/Saml/AuthRequest.php
+++ src/OneLogin/Saml/AuthRequest.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once('Settings.php');
 /**
  * Create a SAML authorization request.
  */
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,13 +2,13 @@
 
 define('TEST_ROOT', __DIR__);
 
-define('XMLSECLIBS_DIR', './../ext/xmlseclibs/');
-require XMLSECLIBS_DIR . 'xmlseclibs.php';
+define('XMLSECLIBS_DIR', '../ext/xmlseclibs/');
+require_once XMLSECLIBS_DIR . 'xmlseclibs.php';
 
-define('ONELOGIN_SAML_DIR', './../src/OneLogin/Saml/');
-require ONELOGIN_SAML_DIR . 'AuthRequest.php';
-require ONELOGIN_SAML_DIR . 'Response.php';
-require ONELOGIN_SAML_DIR . 'Settings.php';
-require ONELOGIN_SAML_DIR . 'XmlSec.php';
+define('ONELOGIN_SAML_DIR', '../src/OneLogin/Saml/');
+require_once ONELOGIN_SAML_DIR . 'AuthRequest.php';
+require_once ONELOGIN_SAML_DIR . 'Response.php';
+require_once ONELOGIN_SAML_DIR . 'Settings.php';
+require_once ONELOGIN_SAML_DIR . 'XmlSec.php';
 
 date_default_timezone_set('America/Los_Angeles');
