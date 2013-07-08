4df6a0827656f86e4ef84c74746b6168e78918b8
marco <marco@studio>
2013-07-08 19:01:31 +0200

    Add the ability of handling encrypted Assertion, as required by the Interoperable SAML 2.0 Web Browser SSO Deployment Profile

--- src/OneLogin/Saml/Response.php
+++ src/OneLogin/Saml/Response.php
@@ -11,10 +11,22 @@ class OneLogin_Saml_Response
     protected $_settings;
 
     /**
-     * The decoded, unprocessed XML assertion provided to the constructor.
+     * The decoded, unprocessed XML message provided to the constructor.
      * @var string
      */
-    public $assertion;
+    protected $assertion;
+    
+    /**
+     * The path of assertion node. It is lazy-initilized in the _findAssertion()
+     * method to speedup queries.
+     * @var string 
+     */
+    private $assertionPath;
+
+    /**
+     * The assertion was encrypted in the response.
+     */
+    public $encrypted = FALSE;
 
     /**
      * A DOMDocument class loaded from the $assertion.
@@ -25,6 +37,12 @@ class OneLogin_Saml_Response
     /**
      * Construct the response object.
      *
+     * Raises exception for basic malformed/incomplete response.
+     * 
+     * According Interoperable SAML 2.0 Web Browser SSO Deployment Profile,
+     * par. 9.2, the response MUST contain exactly one assertion (encrypted or
+     * not).
+     * 
      * @param OneLogin_Saml_Settings $settings Settings containing the necessary X.509 certificate to decode the XML.
      * @param string $assertion A UUEncoded SAML assertion from the IdP.
      */
@@ -34,6 +52,22 @@ class OneLogin_Saml_Response
         $this->assertion = base64_decode($assertion);
         $this->document = new DOMDocument();
         $this->document->loadXML($this->assertion);
+
+        // Check SAML version
+        if ($this->document->documentElement->getAttribute('Version') != '2.0') {
+            throw new Exception('Unsupported SAML version');
+        }
+
+        // Quick check for the presence of EncryptedAssertion
+        $encryptedAssertionNodes = $this->document->getElementsByTagName('EncryptedAssertion');
+        if ($encryptedAssertionNodes->length > 1) {
+            throw new Exception('Multiple encrypted assertions are not supported');
+        }
+        else if ($encryptedAssertionNodes->length == 1) {
+            // Decrypt
+            $this->encrypted = TRUE;
+            $this->_decryptAssertion($this->document);
+        }
     }
 
     /**
@@ -49,6 +83,18 @@ class OneLogin_Saml_Response
     }
 
     /**
+     * Get the URI reference indicating the address to which the response has
+     * been sent or NULL if not address id specified.
+     * If present, the receiver must check it if equals to the spReturnUrl
+     * See SAML core spec. par. 3.2.2.
+     */
+    public function getDestination()
+    {
+        $destination = $this->document->documentElement->getAttribute('Destination');
+        return empty($destination) ? NULL : $destination;
+    }
+    
+    /**
      * Get the NameID provided by the SAML response from the IdP.
      */
     public function getNameId()
@@ -58,7 +104,7 @@ class OneLogin_Saml_Response
     }
 
     /**
-     * Get the SessionNotOnOrAfter attribute, as Unix Epoc, from the
+     * Get the SessionNotOnOrAfter attribute, as Unix Epoch, from the
      * AuthnStatement element.
      * Using this attribute, the IdP suggests the local session expiration
      * time.
@@ -96,6 +142,46 @@ class OneLogin_Saml_Response
     }
 
     /**
+     * Search the assertion node and validate the signature.
+     * Signature is validated only once.
+     */
+    protected function _findAssertion() {
+        if (!empty($this->assertionPath)) {
+            return $this->assertionPath;
+        }
+        
+        $xpath = new DOMXPath($this->document);
+        $xpath->registerNamespace('samlp'   , 'urn:oasis:names:tc:SAML:2.0:protocol');
+        $xpath->registerNamespace('saml'    , 'urn:oasis:names:tc:SAML:2.0:assertion');
+        $xpath->registerNamespace('ds'      , 'http://www.w3.org/2000/09/xmldsig#');
+
+        $assertionNode = $this->encrypted ? '/samlp:Response/saml:EncryptedAssertion/saml:Assertion'
+                : '/samlp:Response/saml:Assertion';
+        $signatureQuery = $assertionNode . '/ds:Signature/ds:SignedInfo/ds:Reference';
+        $assertionReferenceNode = $xpath->query($signatureQuery)->item(0);
+        if (!$assertionReferenceNode) {
+            // is the response signed as a whole?
+            if ($this->encrypted) {
+                throw new Exception('Signed Response with encrypted assertion not supported yet');	
+            }
+            $signatureQuery = '/samlp:Response/ds:Signature/ds:SignedInfo/ds:Reference';
+            $assertionReferenceNode = $xpath->query($signatureQuery)->item(0);
+            if (!$assertionReferenceNode) {
+                throw new Exception('Unable to query assertion or response, no Signature Reference found?');	
+            }
+            $id = substr($assertionReferenceNode->attributes->getNamedItem('URI')->nodeValue, 1);
+
+            $this->assertionPath = "/samlp:Response[@ID='$id']/saml:Assertion";
+        } else {
+            $id = substr($assertionReferenceNode->attributes->getNamedItem('URI')->nodeValue, 1);
+
+            $this->assertionPath = "{$assertionNode}[@ID='$id']";
+        }
+        
+        return $this->assertionPath;
+    }
+    
+    /**
      * @param string $assertionXpath
      * @return DOMNodeList
      */
@@ -106,23 +192,57 @@ class OneLogin_Saml_Response
         $xpath->registerNamespace('saml'    , 'urn:oasis:names:tc:SAML:2.0:assertion');
         $xpath->registerNamespace('ds'      , 'http://www.w3.org/2000/09/xmldsig#');
 
-        $signatureQuery = '/samlp:Response/saml:Assertion/ds:Signature/ds:SignedInfo/ds:Reference';
-        $assertionReferenceNode = $xpath->query($signatureQuery)->item(0);
-        if (!$assertionReferenceNode) {
-        	// is the response signed as a whole?
-        	$signatureQuery = '/samlp:Response/ds:Signature/ds:SignedInfo/ds:Reference';
-        	$assertionReferenceNode = $xpath->query($signatureQuery)->item(0);
-        	if (!$assertionReferenceNode) {
-        		throw new Exception('Unable to query assertion or response, no Signature Reference found?');	
-        	}
-        	$id = substr($assertionReferenceNode->attributes->getNamedItem('URI')->nodeValue, 1);
-        	
-        	$nameQuery = "/samlp:Response[@ID='$id']/saml:Assertion" . $assertionXpath;
-        } else {
-	        $id = substr($assertionReferenceNode->attributes->getNamedItem('URI')->nodeValue, 1);
-	
-	        $nameQuery = "/samlp:Response/saml:Assertion[@ID='$id']" . $assertionXpath;
-        }
+        $nameQuery = $this->_findAssertion() . $assertionXpath;
         return $xpath->query($nameQuery);
     }
+    
+    /**
+     * Decrypt an EncryptedAssertion and replace in the DOM the node content
+     * with the cleartext Assertion.
+     * 
+     * @throws Exception
+     */
+    private function _decryptAssertion($dom) {
+        if (empty($this->_settings->spPrivateKey)) 
+        {
+            throw new Exception("No private key available, check settings");
+        }
+        
+        $objenc = new XMLSecEnc();
+        $encData = $objenc->locateEncryptedData($dom);
+        if (!$encData)
+        {
+            throw new Exception("Cannot locate encrypted assertion");
+        }
+        
+        $objenc->setNode($encData);
+        $objenc->type = $encData->getAttribute("Type");
+        if (!$objKey = $objenc->locateKey())
+        {
+            throw new Exception("Unknown algorithm");
+        }
+
+        $key = NULL;
+        if ($objKeyInfo = $objenc->locateKeyInfo($objKey)) {
+            if ($objKeyInfo->isEncrypted) {
+                $objencKey = $objKeyInfo->encryptedCtx;
+                $objKeyInfo->loadKey($this->_settings->spPrivateKey, FALSE, FALSE);
+                $key = $objencKey->decryptKey($objKeyInfo);
+            }
+        }
+                
+        if (empty($objKey->key)) {
+            $objKey->loadKey($key);
+        }
+       
+        $decrypt = $objenc->decryptNode($objKey, TRUE);
+        if ($decrypt instanceof DOMDocument)
+        {	
+            $this->document = $decrypt;
+        } else {
+            $this->document = $decrypt->ownerDocument;
+        }           
+        
+    }
+    
 }
