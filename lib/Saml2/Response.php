b768ce935ca83e693905e67d83c0175bdecd970f
Sixto Martin <pitbulk@gmail.com>
2014-06-20 14:07:06 +0200

    Undo the last change
    Our namespace is saml not saml2

--- lib/Saml2/Response.php
+++ lib/Saml2/Response.php
@@ -446,7 +446,7 @@ class OneLogin_Saml2_Response
 
             $attributeValues = array();
             foreach ($entry->childNodes as $childNode) {
-                if ($childNode->nodeType == XML_ELEMENT_NODE && ($childNode->tagName === 'saml:AttributeValue' || $childNode->tagName === 'saml2:AttributeValue'  )) {
+                if ($childNode->nodeType == XML_ELEMENT_NODE && $childNode->tagName === 'saml:AttributeValue') {
                     $attributeValues[] = $childNode->nodeValue;
                 }
             }
