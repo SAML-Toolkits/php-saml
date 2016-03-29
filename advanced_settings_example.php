01829e6ec722fe8695855e8ed8291d0f066bab0b
Sixto Martin <pitbulk@gmail.com>
2016-03-29 11:20:48 +0200

    #62 .Set none requestedAuthnContext as default behaviour

--- advanced_settings_example.php
+++ advanced_settings_example.php
@@ -50,7 +50,7 @@ $advancedSettings = array (
         // Set to false and no AuthContext will be sent in the AuthNRequest,
         // Set true or don't present this parameter and you will get an AuthContext 'exact' 'urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport'
         // Set an array with the possible auth context values: array ('urn:oasis:names:tc:SAML:2.0:ac:classes:Password', 'urn:oasis:names:tc:SAML:2.0:ac:classes:X509'),
-        'requestedAuthnContext' => true,
+        'requestedAuthnContext' => false,
 
         // Allows the authn comparison parameter to be set, defaults to 'exact' if
         // the setting is not present.
