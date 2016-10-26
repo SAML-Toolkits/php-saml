e51cd6f798de919ef5c895c7293a43cb469f8004
Carlos López <carlos.lopez@hyve.net>
2016-10-26 14:11:57 +0000

    Default NameId for Logout Request: unspecified

--- lib/Saml2/LogoutRequest.php
+++ lib/Saml2/LogoutRequest.php
@@ -65,9 +65,9 @@ class OneLogin_Saml2_LogoutRequest
                 $nameIdFormat = $spData['NameIDFormat'];
                 $spNameQualifier = null;
             } else {
-                $nameId = $idpData['entityId'];
-                $nameIdFormat = OneLogin_Saml2_Constants::NAMEID_ENTITY;
-                $spNameQualifier = $spData['entityId'];
+                $nameId = null;
+                $nameIdFormat = OneLogin_Saml2_Constants::NAMEID_UNSPECIFIED;
+                $spNameQualifier = null;
             }
 
             $nameIdObj = OneLogin_Saml2_Utils::generateNameId(
