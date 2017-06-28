1ac9a3051d8ea5232e6a88b1376b6f575c13fd1b
Elan Ruusamäe <glen@delfi.ee>
2017-06-28 13:55:57 +0300

    use __DIR__ constant
    otherwise side effect may happen that something else is loaded from
    include_path

--- _toolkit_loader.php
+++ _toolkit_loader.php
@@ -8,8 +8,8 @@ $libDir = __DIR__ . '/lib/Saml2/';
 $extlibDir = __DIR__ . '/extlib/';
 
 // Load composer
-if (file_exists('vendor/autoload.php')) {
-    require 'vendor/autoload.php';
+if (file_exists(__DIR__ .'/vendor/autoload.php')) {
+    require __DIR__ . '/vendor/autoload.php';
 }
 
 // Load now external libs
