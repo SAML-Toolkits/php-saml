f432eb58cd16272fa2a1677b1f18aa5b7fac6688
Lukas Reschke <lukas@statuscode.ch>
2017-10-03 15:56:21 +0200

    Add missing Exception annotation to \OneLogin_Saml2_IdPMetadataParser::parseXML
    Signed-off-by: Lukas Reschke <lukas@statuscode.ch>

--- lib/Saml2/IdPMetadataParser.php
+++ lib/Saml2/IdPMetadataParser.php
@@ -79,6 +79,7 @@ class OneLogin_Saml2_IdPMetadataParser
      * @param string $desiredNameIdFormat   If available on IdP metadata, use that nameIdFormat
      *
      * @return array metadata info in php-saml settings format
+     * @throws \Exception
      */
     public static function parseXML($xml, $entityId = null, $desiredNameIdFormat = null)
     {
