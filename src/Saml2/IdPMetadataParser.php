83b55294c2d9a9680d0cb1eb27343da53d968c6b
Sixto Martin <pitbulk@gmail.com>
2021-05-25 17:47:20 +0200

    Add warning about the use of IdpMetadataParser class. If Metadata URLs are provided by 3rd parties, the URL inputs MUST be validated to avoid issues like SSRF

--- src/Saml2/IdPMetadataParser.php
+++ src/Saml2/IdPMetadataParser.php
@@ -26,6 +26,10 @@ class IdPMetadataParser
     /**
      * Get IdP Metadata Info from URL
      *
+     * This class does not validate in any way the URL that is introduced,
+     * make sure to validate it properly before use it in the parseRemoteXML
+     * method in order to avoid security issues like SSRF attacks.
+     *
      * @param string $url                 URL where the IdP metadata is published
      * @param string $entityId            Entity Id of the desired IdP, if no
      *                                    entity Id is provided and the XML
@@ -43,6 +47,9 @@ class IdPMetadataParser
 
         try {
             $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_PROTOCOLS, CURLPROTO_HTTPS | CURLPROTO_HTTP);
+            curl_setopt($ch, CURLOPT_REDIR_PROTOCOLS, CURLPROTO_HTTPS  | CURLPROTO_HTTP);
+            curl_setopt($ch, CURLOPT_MAXREDIRS, 5);
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
