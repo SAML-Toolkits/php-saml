4bf537cd8162a96362be9efe278a5fe5247c6ab7
ckbkr <56784875+ckbaker10@users.noreply.github.com>
2024-05-27 00:54:43 +0200

    Add parameter to exclude validUntil (#569)
    Add parameter to exclude validUntil

--- src/Saml2/Metadata.php
+++ src/Saml2/Metadata.php
@@ -38,10 +38,11 @@ class Metadata
      * @param array         $contacts      Contacts info
      * @param array         $organization  Organization ingo
      * @param array         $attributes
+     * @param bool          $ignoreValidUntil exclude the validUntil tag from metadata
      *
      * @return string SAML Metadata XML
      */
-    public static function builder($sp, $authnsign = false, $wsign = false, $validUntil = null, $cacheDuration = null, $contacts = array(), $organization = array(), $attributes = array())
+    public static function builder($sp, $authnsign = false, $wsign = false, $validUntil = null, $cacheDuration = null, $contacts = array(), $organization = array(), $attributes = array(), $ignoreValidUntil = false)
     {
 
         if (!isset($validUntil)) {
@@ -161,11 +162,23 @@ ATTRIBUTEVALUE;
 
             $requestedAttributeStr = implode(PHP_EOL, $requestedAttributeData);
             $strAttributeConsumingService = <<<METADATA_TEMPLATE
-<md:AttributeConsumingService index="1">
+
+        <md:AttributeConsumingService index="1">
             <md:ServiceName xml:lang="en">{$sp['attributeConsumingService']['serviceName']}</md:ServiceName>
 {$attrCsDesc}{$requestedAttributeStr}
         </md:AttributeConsumingService>
 METADATA_TEMPLATE;
+        }
+
+        if ($ignoreValidUntil) {
+            $timeStr = <<<TIME_TEMPLATE
+cacheDuration="PT{$cacheDuration}S";
+TIME_TEMPLATE;
+        } else {
+            $timeStr = <<<TIME_TEMPLATE
+validUntil="{$validUntilTime}"
+                     cacheDuration="PT{$cacheDuration}S"
+TIME_TEMPLATE;
         }
 
         $spEntityId = htmlspecialchars($sp['entityId'], ENT_QUOTES);
@@ -173,15 +186,13 @@ METADATA_TEMPLATE;
         $metadata = <<<METADATA_TEMPLATE
 <?xml version="1.0"?>
 <md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata"
-                     validUntil="{$validUntilTime}"
-                     cacheDuration="PT{$cacheDuration}S"
+                     {$timeStr}
                      entityID="{$spEntityId}">
     <md:SPSSODescriptor AuthnRequestsSigned="{$strAuthnsign}" WantAssertionsSigned="{$strWsign}" protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol">
 {$sls}        <md:NameIDFormat>{$sp['NameIDFormat']}</md:NameIDFormat>
         <md:AssertionConsumerService Binding="{$sp['assertionConsumerService']['binding']}"
                                      Location="{$acsUrl}"
-                                     index="1" />
-        {$strAttributeConsumingService}
+                                     index="1" />{$strAttributeConsumingService}
     </md:SPSSODescriptor>{$strOrganization}{$strContacts}
 </md:EntityDescriptor>
 METADATA_TEMPLATE;
