bfe96bc3cc78ff8574f8a6f6a839d84259510720
jmf3658 <jfullmer@austin.utexas.edu>
2024-12-19 15:06:03 -0700

    Typo in ignoreValidUntil breaks metadata

--- src/Saml2/Metadata.php
+++ src/Saml2/Metadata.php
@@ -172,7 +172,7 @@ METADATA_TEMPLATE;
 
         if ($ignoreValidUntil) {
             $timeStr = <<<TIME_TEMPLATE
-cacheDuration="PT{$cacheDuration}S";
+cacheDuration="PT{$cacheDuration}S"
 TIME_TEMPLATE;
         } else {
             $timeStr = <<<TIME_TEMPLATE
